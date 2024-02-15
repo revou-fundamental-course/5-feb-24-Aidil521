6f74c68523230b69060a03a3eec27f674c617779
Aidil M <78016014+Aidil521@users.noreply.github.com>
2024-02-16 01:20:46 +0700

    new update html and js

--- JS/script.js
+++ JS/script.js
@@ -1,99 +1,21 @@
-var val = false;
-
-function toggleNavbar() {
-    const navLinks = document.querySelector('.nav-link');
-    navLinks.classList.toggle('show');
-}
-
-function clearDisplay() {
-    document.getElementById('input-value').value = '';
-    document.getElementById('output-value').value = '';
-    document.getElementById('output-kalkulasi').innerHTML = '';
-}
-
-function reverseDisplay() {
-    if (!val) {
-        document.getElementById('input-label').innerHTML = 'Fahrenheit (&deg;F):';
-        document.getElementById('output-label').innerHTML = 'Celcius (&deg;C):';
-        val = true;
-    }
-    else {
-        document.getElementById('input-label').innerHTML = 'Celcius (&deg;C):';
-        document.getElementById('output-label').innerHTML = 'Fahrenheit (&deg;F):';
-        val = false;
-    }
-    document.getElementById('output-value').value = '';
-    document.getElementById('output-kalkulasi').innerHTML = '';
-}
-
-function calculateDisplay() {
-    let inputValue = document.getElementById('input-value').value;
-    if(!val) {
-        if (inputValue !== '') {
-            let output = inputValue * (9/5) + 32;
-            document.getElementById('output-value').value = parseFloat(output.toFixed(2));
-            if (isNaN(output)) {
-                document.getElementById('output-kalkulasi').innerHTML = 'NaN';
-            }
-            else {
-                document.getElementById('output-kalkulasi').innerHTML = '(' + parseFloat(inputValue) + '&deg;C' + ' * (9/5)) + 32 = ' + parseFloat(output.toFixed(2)) + '&deg;F';
-            }
-        }
-    }
-    else {
-        if (inputValue !== '') {
-            let output = (inputValue - 32) * (5/9);
-            document.getElementById('output-value').value = parseFloat(output.toFixed(2));
-            if (isNaN(output)) {
-                document.getElementById('output-kalkulasi').innerHTML = 'NaN';
-            }
-            else {
-                document.getElementById('output-kalkulasi').innerHTML = '(' + parseFloat(inputValue) + '&deg;F' +' - 32) * (5/9) = '+ parseFloat(output.toFixed(2)) + '&deg;C';
-            }
-        }
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Mendapatkan elemen textarea
-    let restrictedTextarea = document.getElementById('input-value');
-
-    // Menggunakan event listener untuk membatasi input hanya pada angka
-    restrictedTextarea.addEventListener('input', function() {
-        // Menghapus karakter non-angka selain (. dan -) dari teks input
-        let text = restrictedTextarea.value.replace(/[^0-9\-\.]/g, '');
-        let newtext = text.replace(/-+(?=[^\d\.]|$)/g, '-');
-        let newtextarea = newtext.replace(/(?<=\d)[^\d\.]+/g, '');
-        let resttextarea = newtextarea.replace(/(?<=\D|^)\./g, '0.');
-        restrictedTextarea.value = resttextarea;
-    });
-});
-
+// Part navigation start
+const navLinks = document.querySelector('.nav-link');
+const icon = document.querySelector('.bar');
 var toggleRumus = document.getElementById('rumus');
 var toggleKalkulasi = document.getElementById('cara-kalkulasi');
 var displayImage1 = document.getElementById('displayImage1');
 var displayImage2 = document.getElementById('displayImage2');
 
-document.addEventListener('click', function(event) {
-    const navLinks = document.querySelector('.nav-link');
-    const icon = document.querySelector('.bar');
+function toggleNavbar() {
+    navLinks.classList.toggle('show');
+}
 
-    if (!navLinks.contains(event.target) && !icon.contains(event.target)) {
-        navLinks.classList.remove('show');
-    }
+toggleRumus.addEventListener('click', function() {
+    displayImage2.style.display = (displayImage2.style.display === 'none') ? 'block' : 'none';
+});
 
-    let isImageClick1 = event.target === displayImage1;
-    let isImageClick2 = event.target === displayImage2;
-
-    let isToggleButtonClick1 = event.target === toggleKalkulasi;
-    let isToggleButtonClick2 = event.target === toggleRumus;
-
-    if (!isImageClick1 && !isToggleButtonClick1) {
-        displayImage1.style.display = 'none';
-    }
-    if (!isImageClick2 &&!isToggleButtonClick2) {
-        displayImage2.style.display = 'none';
-    }
+toggleKalkulasi.addEventListener('click', function() {
+    displayImage1.style.display = (displayImage1.style.display === 'none') ? 'block' : 'none';
 });
 
 // Menambahkan event listener ke gambar untuk mencegah penyebaran event ke atas
@@ -105,10 +27,70 @@ displayImage2.addEventListener('click', function(event) {
     event.stopPropagation();
 });
 
-toggleRumus.addEventListener('click', function() {
-    displayImage2.style.display = (displayImage2.style.display === 'none') ? 'block' : 'none';
-});
+document.addEventListener('click', function(event) {
+    let isImageClick1 = event.target === displayImage1;
+    let isImageClick2 = event.target === displayImage2;
+    let isToggleButtonClick1 = event.target === toggleKalkulasi;
+    let isToggleButtonClick2 = event.target === toggleRumus;
 
-toggleKalkulasi.addEventListener('click', function() {
-    displayImage1.style.display = (displayImage1.style.display === 'none') ? 'block' : 'none';
+    if (!navLinks.contains(event.target) && !icon.contains(event.target)) {
+        navLinks.classList.remove('show');
+    }
+    if (!isImageClick1 && !isToggleButtonClick1) {
+        displayImage1.style.display = 'none';
+    }
+    if (!isImageClick2 && !isToggleButtonClick2) {
+        displayImage2.style.display = 'none';
+    }
 });
+// Part navigation end
+
+// Part main content start
+var val = false;
+
+function reverseDisplay() { // reverse label value temperature
+    let valTemp = ['Fahrenheit (&deg;F):', 'Celcius (&deg;C):'];
+    (!val) ? val = true : val = false;
+    document.getElementById('input-label').innerHTML = valTemp[1 - val];
+    document.getElementById('output-label').innerHTML = valTemp[0 + val];
+    document.getElementById('output-value').value = '';
+    document.getElementById('output-kalkulasi').innerHTML = '';
+}
+
+function calculateDisplay() { // calculate convert value temperature
+    let output, outputCalculated;
+    let inputValue = document.getElementById('input-value').value;
+    if (inputValue !== '') {
+        if(!val) { // convert celcius to fahrenheit
+            output = inputValue * (9/5) + 32;
+            outputCalculated = '(' + parseFloat(inputValue) + '&deg;C * (9/5)) + 32 = ' + parseFloat(output.toFixed(2)) + '&deg;F';
+        }
+        else {     // convert fahrenheit to celsius
+            output = (inputValue - 32) * (5/9);
+            outputCalculated = '(' + parseFloat(inputValue) + '&deg;F - 32) * (5/9) = ' + parseFloat(output.toFixed(2)) + '&deg;C';
+        }
+    }
+    document.getElementById('output-value').value = parseFloat(output.toFixed(2));
+    document.getElementById('output-kalkulasi').innerHTML = (isNaN(output)) ? outputCalculated = 'NaN' : outputCalculated;
+}
+
+function clearDisplay() { // clear or reset value display input and output
+    document.getElementById('input-value').value = '';
+    document.getElementById('output-value').value = '';
+    document.getElementById('output-kalkulasi').innerHTML = '';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let restrictedTextarea = document.getElementById('input-value'); // Mendapatkan elemen textarea
+
+    // Menggunakan event listener untuk membatasi input hanya pada angka
+    restrictedTextarea.addEventListener('input', function() {
+        // Menghapus karakter non-angka selain (. dan -) dari teks input
+        let text = restrictedTextarea.value.replace(/[^0-9\-\.]/g, '');
+        let newtext = text.replace(/-+(?=[^\d\.]|$)/g, '-');
+        let newtextarea = newtext.replace(/(?<=\d)[^\d\.]+/g, '');
+        let resttextarea = newtextarea.replace(/(?<=\D|^)\./g, '0.');
+        restrictedTextarea.value = resttextarea;
+    });
+});
+// Part main content end
